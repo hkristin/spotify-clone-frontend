aee65877e2fd3262f32c8976634f7479b04ccb06
Kristin239 <hkristin@andrew.cmu.edu>
2020-10-17 22:23:15 -0400

    moved router code to home component; added links

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Artists from './Artists'
+import Songs from './Songs'
+import Playlists from './Playlists'
+import PlaylistNew from './PlaylistNew'
 
 class Home extends Component {
   render() {
     return (
       <div id="home-container">
-        <h1>Welcome To My SPotify Clone App!</h1>
-        <a className="home-page-link" href="/artists">View Artists</a> <br />
-        <a className="home-page-link" href="/songs">View Songs</a> <br />
-        <a className="home-page-link" href="/playlists">View Playlists</a> <br />
-        <a className="home-page-link" href="/playlists/new">Add New Playlist</a> <br />
+        <h1>Welcome To My Spotify Clone App!</h1>
+        <Router>
+        <Switch>
+            <Route exact path="/artists" component={ Artists } />
+            <Route exact path="/songs" component={ Songs } />
+            <Route exact path="/playlists" component={ Playlists } />
+            <Route exact path="/playlists/new" component={ PlaylistNew } />
+          </Switch>
+        </Router>
+
+        <a href="/artists" >View Artists</a> <br />
+        <a href="/songs" >View Songs</a> <br />
+        <a href="/playlists" >View Playlists</a> <br />
+        <a href="/playlists/new" >New Playlist</a>
       </div>
     )
   }
